ff4c3fc221fceecb91626b170aa58c1a8a3bf0ec
saidsahinbas <saidsahinbas@gmail.com>
2024-03-23 21:29:21 +0300

    Relation between course and tutor

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,9 @@
+const moment = require('moment');
+
 const User = require('../models/User');
 const Category = require('../models/Category');
 const bcrypt = require('bcrypt');
+const Course = require('../models/Course');
 
 exports.createUser = async (req, res) => {
     try {
@@ -47,9 +50,11 @@ exports.logoutUser = (req, res) => {
 exports.getDashboardPage = async (req, res) => {
     const user = await User.findOne({ _id: req.session.userID });
     const categories = await Category.find({});
+    const courses = await Course.find({ user: req.session.userID });
     res.status(200).render('dashboard', {
         page_name: 'dashboard',
         categories,
-        user
+        user,
+        courses
     });
 };
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,9 +1,15 @@
 const Course = require('../models/Course');
 const Category = require('../models/Category');
+const moment = require('moment');
 
 exports.createCourse = async (req, res) => {
     try {
-        const course = await Course.create(req.body);
+        const course = await Course.create({
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            user: req.session.userID
+        });
 
         res.status(201).redirect('/courses')
     } catch (error) {
@@ -43,12 +49,14 @@ exports.getAllCoureses = async (req, res) => {
 
 exports.getCourseById = async (req, res) => {
     try {
-        const course = await Course.findOne({ slug: req.params.slug });
+        const course = await Course.findOne({ slug: req.params.slug }).populate('user');
+        const month = moment(course.createdAt).format("MMMM");
         const categories = await Category.find({});
         res.status(200).render('course-single', {
             course,
             page_name: 'courses',
-            categories
+            categories,
+            month
         });
 
 
