6984f9ba79898d455bc6d79e39a03115158574e1
saidsahinbas <saidsahinbas@gmail.com>
2024-03-25 22:50:11 +0300

    Validation Register and Login User

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const { body } = require('express-validator');
+
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const User = require('../models/User');
+
 const router = express.Router();
 
-router.route('/signup').post(authController.createUser);
+router.route('/signup').post(
+    [
+        body('name').not().isEmpty().withMessage('Please enter your name '),
+        body('email').not().isEmpty().withMessage('Please enter your email ')
+            .custom((userEmail) => {
+                return User.findOne({ email: userEmail }).then((user) => {
+                    if (user) {
+                        return Promise.reject('Email is already exists');
+                    }
+                })
+            }),
+        body('password').not().isEmpty().withMessage('Please enter your password '),
+    ],
+    authController.createUser);
+
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
 router.route('/dashboard').get(authMiddleware, authController.getDashboardPage);
